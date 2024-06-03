b7834c4af372cc0d81a37486ce2a7f7cddcc9ea0
abdullahprince7717 <abdullahprince7717@gmail.com>
2024-06-03 19:48:18 +0500

    practised rxjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import exphbs from 'express-handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  
+  // Set up the views directory and the Handlebars engine
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.engine('hbs', exphbs());
+  app.setViewEngine('hbs');
   await app.listen(3001);
 }
 bootstrap();
